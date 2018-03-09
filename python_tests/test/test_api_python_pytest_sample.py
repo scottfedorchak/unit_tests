4bac3160e44798bcbbe8907ee480f1f4589e12cd
scottfedorchak <scottfedorchak@gmail.com>
2018-03-08 16:44:20 -0800

    small change to the way set_up fixture is used for pytest

--- python_tests/test/test_api_python_pytest_sample.py
+++ python_tests/test/test_api_python_pytest_sample.py
@@ -4,11 +4,11 @@ import urllib2
 
 class TestApiPythonPytestSample:
     @pytest.fixture
-    def setUp(self):
+    def set_up(self):
         self.ApiUrl = "http://jsonplaceholder.typicode.com/posts"
 
-    @pytest.mark.skip
-    def test_not_empty(self, setUp):
+    @pytest.mark.usefixtures("set_up")
+    def test_not_empty(self):
         # define api response
         testurl = (self.ApiUrl + '/1')
         print testurl
@@ -21,7 +21,8 @@ class TestApiPythonPytestSample:
         assert json_response != "{}"
         assert "userId" in json_response
 
-    def test_404_not_found_exception(self,setUp):
+    @pytest.mark.usefixtures("set_up")
+    def test_404_not_found_exception(self):
         with pytest.raises(Exception) as excinfo:
             testurl = (self.ApiUrl + '/zzzzzzzzz')
             response = urllib2.urlopen(testurl)
